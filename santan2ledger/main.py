647661a341c84bf554265d59fbdbd583a74c3687
isaacjeffersonlee <27ilee01@gmail.com>
2022-08-24 20:26:57 +0100

    add better pickup where you left off

--- santan2ledger/main.py
+++ santan2ledger/main.py
@@ -6,6 +6,12 @@ from santan2ledger.selector import Selector
 from santan2ledger.xact import Xact
 import santan2ledger.colors as colors
 
+# TODO: Use last row matching instead of date matching
+# TODO: Add title page
+# TODO: Polish UI
+# TODO: Package for installation and portability
+# TODO: Add README.md
+
 
 MODULE_PATH = os.path.abspath(
     os.path.join(os.path.dirname(__file__), "../santan2ledger")
@@ -13,7 +19,7 @@ MODULE_PATH = os.path.abspath(
 ROOT_PATH = os.path.dirname(MODULE_PATH)
 
 
-def main(statement_file_name: str) -> None:
+def main(statement_file_name: str, date_after: str) -> None:
     try:
         # Define objects used for printing, selecting and parsing
         selector = Selector(
@@ -22,26 +28,7 @@ def main(statement_file_name: str) -> None:
         parser = Parser(ROOT_PATH + "/config.json")
         # Backup ledger and accounts file
         parser.make_backup()
-        # Read in statements from .txt file
-        statement_file_path = parser.statements_dir + statement_file_name
-        # Also reverse the order, so that earliest transactions appear first
-        statement_df = parser.txt_to_df(file_path=statement_file_path).iloc[::-1]
-        if not selector.prev_xact_df.empty:
-            last_recorded_date = (
-                pd.to_datetime(selector.prev_xact_df["date_str"], format="%Y/%m/%d")
-                .tail(1)
-                .item()
-            )
-            print(f"Last recorded date: {colors.magenta(last_recorded_date)})")
-            # Only consider transactions entered after last_recorded_date
-            statement_df = statement_df.loc[statement_df["Date"] > last_recorded_date]
-        if statement_df.empty:
-            print(colors.red("No statements found!"))
-            return None
-        # Get list of already defined accounts
-        print(
-            f"{colors.green(str(statement_df.shape[0]))} transactions found in statements file..."
-        )
+        # Get list of previously defined accounts from accounts.ledger
         prev_accounts = parser.get_account_list()
         source_account = selector.autocomplete_prompt(
             items=prev_accounts, message="Source Account: "
@@ -49,6 +36,35 @@ def main(statement_file_name: str) -> None:
         default_commodity = selector.autocomplete_prompt(
             items=["GBP", "CHF"], message="Default commodity: "
         )
+        # Read in statements from .txt file
+        statement_file_path = parser.statements_dir + statement_file_name
+        # Also reverse the order, so that earliest transactions appear first
+        statement_df = parser.txt_to_df(file_path=statement_file_path).iloc[::-1]
+        if date_after:
+            statement_df = statement_df.loc[
+                statement_df["Date"]
+                > pd.to_datetime(date_after.replace("-", "/"), format="%d/%m/%Y")
+            ]
+        elif not selector.prev_xact_df.empty:
+            last_recorded_date = ( # TODO: Use last row matching instead of date matching
+                pd.to_datetime(
+                    selector.prev_xact_df.loc[
+                        selector.prev_xact_df["source_account"] == source_account
+                    ]["date_str"],
+                    format="%Y/%m/%d",
+                )
+                .tail(1)
+                .item()
+            )
+            print(f"Last recorded date: {colors.magenta(str(last_recorded_date))})")
+            # Only consider transactions entered after last_recorded_date
+            statement_df = statement_df.loc[statement_df["Date"] > last_recorded_date]
+
+        if statement_df.empty:
+            print(colors.red("No statements found!"))
+            return None
+        # Get list of already defined accounts
+        print(f"{colors.green(str(statement_df.shape[0]))} transactions found...")
         new_xacts = []
 
         idx = 0
@@ -100,6 +116,30 @@ def main(statement_file_name: str) -> None:
         )
     except KeyboardInterrupt:
         print("Detected KeyboardInterrupt, quitting...")
+        while True:
+            save_progress = input(
+                f"Save progress? [{colors.green('y')}]/[{colors.red('n')}]: "
+            )
+            if save_progress not in ("y", "n"):
+                print(f"{save_progress} is not a valid option! Please type y or n...")
+                continue
+            elif save_progress == "y":
+                selector.update_prev_xact_file()
+
+                parser.append_xacts_to_ledger_file(new_xacts)
+                # Only add accounts not originally in accounts.ledger
+                parser.append_accounts_to_file(
+                    list(
+                        selector.new_accounts.difference(set(prev_accounts)).difference(
+                            {"", "k", "p"}
+                        )
+                    )
+                )
+                print(f"Progress saved! {colors.green('FINISHED')}...")
+                break
+            else:
+                print(f"Exiting! Progress {colors.red('NOT')} saved!")
+                break
 
 
 if __name__ == "__main__":
@@ -111,9 +151,15 @@ if __name__ == "__main__":
         dest="statements_file_name",
         help="Santander exported txt file to parse.",
     )
+    parser.add_argument(
+        "-d",
+        "--date-after",
+        dest="date_after",
+        help="Date to start parsing transactions after.",
+    )
     args = parser.parse_args()
 
-    main(args.statements_file_name)
+    main(args.statements_file_name, args.date_after)
 
     # statement_file_name = "Statements09012964141909.txt"
     # main(statement_file_name)
