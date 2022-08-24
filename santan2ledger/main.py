6404cc68d311dcf978d468d2a6623e746ac4dc7d
isaacjeffersonlee <27ilee01@gmail.com>
2022-08-24 04:11:29 +0100

    fix duplicate accounts getting appended

--- santan2ledger/main.py
+++ santan2ledger/main.py
@@ -19,15 +19,18 @@ def main(statement_file_name: str) -> None:
     statement_file_path = parser.statements_dir + statement_file_name
     statement_df = parser.txt_to_df(file_path=statement_file_path)
     # Get list of already defined accounts
-    account_list = parser.get_account_list()
-    print("Select source account: ")
-    source_account = selector.autocomplete_prompt(items=account_list)
+    prev_accounts = parser.get_account_list()
+    source_account = selector.autocomplete_prompt(
+        items=prev_accounts, message="Source Account: "
+    )
     default_commodity = selector.autocomplete_prompt(
         items=["GBP", "CHF"], message="Default commodity: "
     )
+    new_xacts = []
 
     idx = 0
     while True:
+        os.system("clear")
         row = statement_df.iloc[idx]
         xact = Xact(
             source_account=source_account,
@@ -36,26 +39,32 @@ def main(statement_file_name: str) -> None:
             date_str=str(row["Date"].date()),
             commodity=row["Commodity"] if row["Commodity"] else default_commodity,
         )
-        prompt_input = selector.get_target_account(
+        input_str = selector.get_target_account(
             xact=xact,
-            prev_account_list=account_list,
+            prev_account_list=prev_accounts,
             progress=f"Account No. {idx + 1} / {statement_df.shape[0]}",
         )
-        if prompt_input == "k" or prompt_input == "p":
-            idx -= 2  # Go to previous transaction
+        if input_str == "p" or input_str == "k":
+            new_xacts.pop()
+            idx -= 1
+            continue
         else:
-            xact.target_account = prompt_input
+            xact.target_account = input_str
+            selector.append_xact(xact)
+            new_xacts.append(xact)
+            idx += 1
 
-        selector.append_xact(xact)
-        idx += 1
-        if idx == statement_df.shape[0]:
-            print("Finished!")
-            break  # TODO: Add keybinding to quit
+            if idx == statement_df.shape[0]:
+                print("Finished!")
+                break  # TODO: Add keybinding to quit
 
     selector.update_prev_xact_file()
 
-    parser.append_xacts_to_ledger_file(selector.new_xacts)
-    parser.append_accounts_to_file(list(selector.new_accounts))
+    parser.append_xacts_to_ledger_file(new_xacts)
+    # Only add accounts not originally in accounts.ledger
+    parser.append_accounts_to_file(
+        list(selector.new_accounts.difference(set(prev_accounts)))
+    )
 
 
 if __name__ == "__main__":
