89350837001fe11e8aefb1ec26601ef25cb6a6b1
isaacjeffersonlee <27ilee01@gmail.com>
2022-08-25 15:40:56 +0100

    add entry point install method

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 from setuptools import setup, find_packages
+
 setup(
     name="santan2ledger",
     version="0.1",
-    packages=find_packages()
+    scripts=["santan2ledger/main.py"],
+    entry_points={
+        "console_scripts": [
+            "s2l = santan2ledger:main",
+        ],
+    },
+    packages=find_packages(),
 )
